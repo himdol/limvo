6077dd31432273df64172637e565a90adffc978d
himdol <ihimchan746@gmail.com>
2022-08-21 13:50:45 +0900

    리엑트 부트스트랩 연결 테스트 완료 및 indexController 수정

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -1,20 +1,39 @@
 import React, {useEffect, useState} from 'react';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import { Button, ButtonGroup, Dropdown } from "react-bootstrap";
 import axios from 'axios';
 
 function App() {
-  const [hello, setHello] = useState('')
+    const [hello, setHello] = useState('')
 
-  useEffect(() => {
+    useEffect(() => {
     axios.get('/api/index')
         .then(response => setHello(response.data))
         .catch(error => console.log(error))
-  }, []);
+    }, []);
 
-  return (
-      <div>
-        백엔드에서 가져온 데이터입니다 : {hello}
-      </div>
-  );
+    return (
+        <>
+            <div>
+                백엔드에서 가져온 데이터입니다 : {hello}
+            </div>
+            <div>
+                <Button>이거됨?</Button>
+            </div>
+            <Form>
+                <Row>
+                    <Col>
+                        <Form.Control placeholder="First name" />
+                    </Col>
+                    <Col>
+                        <Form.Control placeholder="Last name" />
+                    </Col>
+                </Row>
+            </Form>
+        </>
+    );
 }
 
 export default App;
