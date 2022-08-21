fa6b2d14d0109996be3bdce002ddca798d0c3e0f
himdol <ihimchan746@gmail.com>
2022-08-21 12:32:47 +0900

    시작

---
+++ src/main/java/com/limvo/toyproejct/ToyproejctApplication.java
@@ -0,0 +1,13 @@
+package com.limvo.toyproejct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyproejctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyproejctApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/limvo/toyproejct/ToyproejctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limvo.toyproejct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyproejctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
